ed70dcadd228678af184930dc57aa6d9b652c276
PythonIA <90198356+PythoniaES@users.noreply.github.com>
2021-12-30 20:03:31 +0100

    Create data_downloader.py

---
+++ FastLabel/data_downloader.py
@@ -0,0 +1,36 @@
+from binance.client import Client
+import pandas as pd
+from datetime import datetime, timedelta
+import os
+
+BINANCE = {
+  "key": "",
+  "secret": ""
+}
+
+
+client = Client(BINANCE['key'], BINANCE['secret'])
+
+def parser(data):
+    df = pd.DataFrame(data)
+    df = df.drop([6, 7, 8, 9, 10, 11], axis=1)
+
+    col_names = ['time', 'open', 'high', 'low', 'close', 'volume']
+    df.columns = col_names
+    for col in col_names:
+        df[col] = df[col].astype(float)
+    df['start'] = pd.to_datetime(df['time'] * 1000000)
+
+    return df
+
+def last_data(exchange):
+    if not os.path.exists('./DATA'):
+        os.mkdir('./DATA')
+    candles = client.get_historical_klines(symbol=exchange, interval='30m', start_str=str(datetime.now() - timedelta(days=365)) )
+    df = parser(candles)
+    df.to_csv('./DATA/{}.csv'.format(exchange),index=False)
+
+if __name__ == '__main__':
+
+    for i in ['BTCEUR','ADAEUR','ETHEUR','LUNAEUR','AVAXEUR','SOLEUR','XRPEUR','DOTEUR','DOGEEUR']:
+        last_data(i)
