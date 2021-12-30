2c5ced2116ee6a202c36f9981c3e01ec81d9c041
PythonIA <90198356+PythoniaES@users.noreply.github.com>
2021-12-30 15:47:44 +0100

    Update data_downloader.py

--- FastLabel/data_downloader.py
+++ FastLabel/data_downloader.py
@@ -1,11 +1,11 @@
 from binance.client import Client
 import pandas as pd
 from datetime import datetime, timedelta
-
+import os
 
 BINANCE = {
-  "key": "",
-  "secret": ""
+  "key": "3twyIVmgSoinkU39hx2Mubt8bREk3O2k2lSfw29LNDfw7u57twEUNQLbvQiJBTVu",
+  "secret": "AY01aIZp5yB77sfXnqx34xDvz9xdq0HvZK6evRFze8QWAEdYG5uyDRPsjocUW3Mv"
 }
 
 
@@ -24,12 +24,13 @@ def parser(data):
     return df
 
 def last_data(exchange):
-
+    if not os.path.exists('./DATA'):
+        os.mkdir('./DATA')
     candles = client.get_historical_klines(symbol=exchange, interval='30m', start_str=str(datetime.now() - timedelta(days=365)) )
     df = parser(candles)
     df.to_csv('./DATA/{}.csv'.format(exchange),index=False)
 
 if __name__ == '__main__':
 
-    for i in ['BTCUSDT','ETHUSDT']:
+    for i in ['BTCEUR','ADAEUR','ETHEUR','LUNAEUR','AVAXEUR','SOLEUR','XRPEUR','DOTEUR','DOGEEUR']:
         last_data(i)
